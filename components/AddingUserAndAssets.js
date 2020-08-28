81e39e326e9617a9d5ff01df0a56908b632ff0e1
Rohit Kumar Mishra <rohit.mishra@leadnorthtech.com>
2020-08-28 17:16:00 +0530

    list added

--- components/AddingUserAndAssets.js
+++ components/AddingUserAndAssets.js
@@ -7,14 +7,17 @@ import {
   TouchableOpacity,
   ScrollView,
   Picker,
+  FlatList,
 } from "react-native";
-import { Icon } from "native-base";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { Icon } from "native-base";
 
 export default function AddingUserAndAssets() {
+  const [value, onChangeText] = useState();
+
   return (
     <ScrollView pagingEnabled>
       <View style={styles.header}>
@@ -26,8 +29,147 @@ export default function AddingUserAndAssets() {
           <Text style={styles.text}>CREATE A NEW TASK</Text>
         </View>
       </View>
-      <View>
+      <View
+        style={{
+          borderBottomWidth: StyleSheet.hairlineWidth,
+          borderBottomColor: "#c0c0c0",
+        }}
+      >
         <Text style={styles.taskDetailsText}>Add User</Text>
+        <Text style={styles.taskNameText}>SUPERVISOR</Text>
+        <View style={styles.supervisor}>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Raj Anand</Text>
+          <TouchableOpacity
+            onPress={() => {
+              console.log("Change button");
+            }}
+          >
+            <Text style={styles.listtext}>Change</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.taskNameText}>SEARCH</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => onChangeText(text)}
+            value={value}
+            placeholder={"Search for workers to add"}
+          ></TextInput>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              console.log("Invite Worker");
+            }}
+          >
+            <Text style={styles.inviteWorker}>Invite New worker</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ padding: 20 }}>
+          <FlatList
+            // data={["Devin", "Den", "Dominc", "Jack"]}
+            data={[
+              { name: "Worker a", action: "Add" },
+              { name: "Worker b", action: "Remove" },
+              { name: "Worker c", action: "Add" },
+              { name: "Worker d", action: "Remove" },
+              { name: "Worker e", action: "Add" },
+            ]}
+            renderItem={({ item }) => (
+              <View style={styles.listview}>
+                <Text style={styles.listtext}>{item.name}</Text>
+                <TouchableOpacity
+                  onPress={() => {
+                    console.log("Action button");
+                  }}
+                >
+                  <Text style={styles.listtext}>{item.action}</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+      {/* Add Assets */}
+      <View
+        style={{
+          borderBottomWidth: StyleSheet.hairlineWidth,
+          borderBottomColor: "#c0c0c0",
+        }}
+      >
+        <Text style={styles.taskDetailsText}>Add Assets</Text>
+        <View>
+          <Text style={styles.taskNameText}>SEARCH</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => onChangeText(text)}
+            value={value}
+            placeholder={"Search for assets to add"}
+          ></TextInput>
+        </View>
+        <View style={{ padding: 20 }}>
+          <FlatList
+            // data={["Devin", "Den", "Dominc", "Jack"]}
+            data={[
+              { name: "Asset a", action: "Add" },
+              { name: "Asset b", action: "Remove" },
+              { name: "Asset c", action: "Add" },
+              { name: "Asset d", action: "Remove" },
+            ]}
+            renderItem={({ item }) => (
+              <View style={styles.listview}>
+                <Text style={styles.listtext}>{item.name}</Text>
+                <TouchableOpacity
+                  onPress={() => {
+                    console.log("Action button");
+                  }}
+                >
+                  <Text style={styles.listtext}>{item.action}</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+      {/* Task Details */}
+      <View
+        style={{
+          marginTop: 15,
+          borderTopColor: "#c0c0c0",
+          borderTopWidth: StyleSheet.hairlineWidth,
+        }}
+      >
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            margin: 20,
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>TASK 01</Text>
+            <Text>AD HOC</Text>
+          </View>
+          <Text style={styles.listtext}>Collapse</Text>
+        </View>
+        <View
+          style={{
+            marginTop: 15,
+            borderTopColor: "#c0c0c0",
+            borderTopWidth: StyleSheet.hairlineWidth,
+          }}
+        >
+          <View style={{ flexDirection: "row", margin: 20 }}>
+            <View>
+              <Text style={{ color: "#c0c0c0", fontSize: 12 }}>SHIFT</Text>
+              <Text style={{ fontSize: 14 }}>Morning</Text>
+            </View>
+            <View style={{ marginLeft: 30 }}>
+              <Text style={{ color: "#c0c0c0", fontSize: 12 }}>ROUTE</Text>
+              <Text style={{ fontSize: 14 }}>Route 24</Text>
+            </View>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -55,5 +197,41 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginTop: 20,
     fontWeight: "bold",
+  },
+  taskNameText: {
+    left: 20,
+    padding: 5,
+
+    color: "#c0c0c0",
+    marginTop: 10,
+  },
+  textInput: {
+    height: hp("7%"),
+    borderColor: "#c0c0c0",
+    borderWidth: 2,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    paddingLeft: 20,
+  },
+  supervisor: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+  inviteWorker: {
+    color: "#1E90FF",
+    marginLeft: 20,
+    marginTop: 10,
+    fontWeight: "bold",
+  },
+  listview: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  listtext: {
+    color: "#1E90FF",
+    fontWeight: "bold",
+    marginVertical: 10,
+    fontSize: 15,
   },
 });
